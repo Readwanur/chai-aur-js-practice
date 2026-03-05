56fc76a375c91e3fa31a51e767816adc6e8390b8
Md. Readwanur Rahman <163653105+Readwanur@users.noreply.github.com>
2026-03-05 14:29:04 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+let number = 23
+let isLoggedIn = false
+let name = "readwanur rahman"
+let state;
+
+// number => 2 to the power 53
+// BigInt
+// string => "something"
+// boolean => true/false
+// null => standalone value (Object)
+// undefined => undefined
+
+// Object
+
+console.log(typeof '');
+console.log(typeof null);
+
