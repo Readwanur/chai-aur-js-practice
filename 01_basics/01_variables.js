f045e5fbd8ffd930ad45fe6f268a5c09f90080db
Md. Readwanur Rahman <163653105+Readwanur@users.noreply.github.com>
2026-03-05 12:04:10 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 784
+let accountEmail = 'rrumon710@gmail.com'
+var accountPassword = '2121'
+accountCity = 'Dhaka'  //We can also declare like this but not a good practice
+
+let accountState; // if dont store any value just declare the variable like this it will be taken as undefined
+
+// accountId = 791 // Not allowed // We cant change the value of Const DataTypes
+
+accountEmail = 'melodaygangster@gmail.com'
+accountPassword = '4343'
+accountCity = 'Bogura'
+
+console.log(accountId);
+
+console.table([accountEmail, accountCity, accountPassword, accountState]);
